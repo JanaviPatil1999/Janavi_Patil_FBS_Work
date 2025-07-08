7f81d3a1c08d26e9d34498cc02d40afea05e045c
JanaviPatil1999 <151437257+JanaviPatil1999@users.noreply.github.com>
2025-07-08 14:52:23 +0530

    Add files via upload

---
+++ C Programming/Assignment5/diagonalWithRect.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+	int row ,column;
+	printf("Enter the number: ");
+	scanf("%d",&row);
+	printf("Enter the number: ");
+	scanf("%d",&column);
+	
+	for(int i=0;i<row;i++){
+		for(int j=0;j<column;j++){
+			if(i==0||i==row-1||j==0||j==column-1||i==j){
+					printf("*");
+			}else{
+				printf(" ");
+			}
+		
+		}
+		printf("\n");
+	}
+}
